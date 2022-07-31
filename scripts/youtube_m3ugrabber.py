817da39cee4db72d38792154ab94340a0c701366
Optickal <awelisch96@web.de>
2022-08-01 01:40:24 +0200

    Update youtube_m3ugrabber.py

--- scripts/youtube_m3ugrabber.py
+++ scripts/youtube_m3ugrabber.py
@@ -138,13 +138,6 @@ with open('../youtube_channel_info.txt') as f:
             tvg_logo = line[2].strip()
             tvg_id = line[3].strip()
             print(f'\n#EXTINF:-1 group-title="{grp_title}" tvg-logo="{tvg_logo}" tvg-id="{tvg_id}", {ch_name}')
-        if line.endswith('info.m3u8')
-            line = line.split('|')
-            ch_name = line[0].strip()
-            grp_title = line[1].strip().title()
-            tvg_logo = line[2].strip()
-            tvg_id = line[3].strip()
-            print(f'\n#EXTINF:-1 group-title="{grp_title}" tvg-logo="{tvg_logo}" tvg-id="{tvg_id}", Offline - {ch_name}')
         else:
             grab(line)
             
